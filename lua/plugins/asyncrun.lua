146a88c70752ef0d98bfb6d98084a03778ef90cf
AntonioPaess <paesdeandradeantonio@gmail.com>
2025-04-04 16:04:26 -0300

    lsp and none-ls configuration

--- lua/plugins/asyncrun.lua
+++ lua/plugins/asyncrun.lua
@@ -1,10 +1,9 @@
 return {
-  {
-    "skywind3000/asyncrun.vim",
-    lazy = false, -- Pode ser true se não precisa carregar imediatamente
-    config = function()
-      -- Configuração opcional do asyncrun, se necessário
-    end
-  }
+	{
+		"skywind3000/asyncrun.vim",
+		lazy = false, -- Pode ser true se não precisa carregar imediatamente
+		config = function()
+			-- Configuração opcional do asyncrun, se necessário
+		end,
+	},
 }
-
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,33 +1,30 @@
-return{
+return {
+
   {
     "williamboman/mason.nvim",
-       config = function()
+    config = function()
       require("mason").setup()
     end
   },
   {
-   "williamboman/mason-lspconfig.nvim",
-  config = function()
-    require("mason-lspconfig").setup({
-        ensure_installe = { "lua_ls"}
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "clangd" }
       })
     end
   },
   {
     "neovim/nvim-lspconfig",
     config = function()
-local lspconfig = require("lspconfig")
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.clangd.setup({})
 
-lspconfig.lua_ls.setup({
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { "vim" }
-      }
-    }
-  }
-})
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, {})
     end
   }
-}
 
+}
